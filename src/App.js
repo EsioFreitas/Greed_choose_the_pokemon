9d82f6d1e1e204dd9c57e1c382145a02da78adde
EsioFreitas <esio.gustavo@gmail.com>
2020-10-15 06:51:14 -0300

    Give feedback

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
   const [capturedPokemons, setCapturedPokemons] = useState([]);
   const [pokemonsPoints, setPokemonsPoints] = useState(0);
-  const [result, setResult] = useState([]);
+  const [exchangedPokemons, setExchangedPokemons] = useState([]);
   const [couldAnswer, setCouldAnswer] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
   const [chosenPokemon, setChosenPokemon] = useState([]);
 
   useEffect(() => {
@@ -38,46 +39,59 @@ function App() {
   const choosePokemon = (pokemon) => {
     const newInfo = [...chosenPokemon, pokemon];
     setChosenPokemon(newInfo);
-  }
+  };
 
   const calculateValueToTrade = () => {
     let valueToTrade = 0;
 
-    chosenPokemon.forEach(pokemon => valueToTrade += pokemon.rarity);
+    chosenPokemon.forEach((pokemon) => (valueToTrade += pokemon.rarity));
 
     return valueToTrade;
-  }
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+    setTimeout(() => {
+      setExchangedPokemons([]);
+      setChosenPokemon([]);
+    }, 500);
+  };
 
   const evaluatePokemons = () => {
     let valueToTrade = calculateValueToTrade();
-    let sortedPokemons = [...capturedPokemons]
+    let sortedPokemons = [...capturedPokemons];
     let couldAnswer = true;
 
     sortedPokemons.sort((a, b) => {
-      if (a.rarity < b.rarity) return 1
-      if (a.rarity > b.rarity) return -1
-      return 0
-    })
+      if (a.rarity < b.rarity) return 1;
+      if (a.rarity > b.rarity) return -1;
+      return 0;
+    });
 
-    const result = []
+    const result = [];
 
-    while(valueToTrade > 0) {
-      const index = sortedPokemons.findIndex(pokemon => pokemon.rarity <= valueToTrade);
+    while (valueToTrade > 0) {
+      const index = sortedPokemons.findIndex(
+        (pokemon) => pokemon.rarity <= valueToTrade
+      );
       if (index >= 0) {
         result.push(sortedPokemons[index]);
-        valueToTrade -= sortedPokemons[index].rarity
+        valueToTrade -= sortedPokemons[index].rarity;
         sortedPokemons.splice(index, 1);
       } else {
         valueToTrade = 0;
         couldAnswer = false;
-        console.log('Não é possível fazer uma troca exata')
+        console.log(sortedPokemons);
+        console.log("Não é possível fazer uma troca exata");
       }
     }
-    if(couldAnswer) {
-      setResult(result);
-      console.log(result)
+    if (couldAnswer) {
+      setExchangedPokemons(result);
+      console.log(result);
     }
-  }
+
+    setOpenModal(true);
+  };
 
   return (
     <div className="bg-light" style={{ height: "100vh" }}>
@@ -89,21 +103,15 @@ function App() {
               Escolha o Pokemon
             </span>
           </h1>
-          <p>History</p>
+          <p>
+            O professor carvalho está trocando pokémons, mas para a troca ser
+            justa, ele está usando a raridade dos pokémons como critério.
+            Escolha quais pokémons você quer e vamos te dizer qual é o menor
+            número de pokémons que você precisa transferir para trocar.
+          </p>
         </div>
         <div className="row">
           <div className="col">
-            <div className="row">
-              <div className="col mb-3">
-                <div className="card">
-                  <div className="card-body p-2">
-                    <h6 className="card-title text-center m-0 p-0">
-                      {`Pontuação da pokedex: ${pokemonsPoints}`}
-                    </h6>
-                  </div>
-                </div>
-              </div>
-            </div>
             <div className="row">
               <div className="col">
                 <div className="pokemons-grid">
@@ -129,29 +137,6 @@ function App() {
           <div className="col">
             <div className="row">
               <div className="col">
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Nome do pokemon"
-                    aria-label="Nome do pokemon"
-                    aria-describedby="button-addon2"
-                    onChange={(e) => searchPokemon(e.target.value)}
-                  />
-                  <div className="input-group-append">
-                    <button
-                      className="btn btn-outline-primary"
-                      type="button"
-                      id="button-addon2"
-                    >
-                      Pesquisar
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
                 <div className="pokemons-grid">
                   {pokemons.map((pokemon) => (
                     <div key={pokemon.id} className="card p-2">
@@ -165,7 +150,10 @@ function App() {
                           {`${pokemon.name} - ${getPokemontPoints(pokemon)}`}
                         </h6>
                       </div>
-                      <button onClick={() => choosePokemon(pokemon)} className="btn btn-primary">
+                      <button
+                        onClick={() => choosePokemon(pokemon)}
+                        className="btn btn-primary"
+                      >
                         Escolher
                       </button>
                     </div>
@@ -175,11 +163,89 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="row ">
-          <div className="col mt-4 d-flex justify-content-center">
-            <button onClick={evaluatePokemons} className="btn btn-primary w-50"  type="button">
+
+        <div className="row my-4">
+          <div className="col">
+            <div
+              className=" d-flex justify-content-beetwen flex-nowrep "
+              style={{ overflow: "auto" }}
+            >
+              {chosenPokemon.map((pokemon) => (
+                <div
+                  key={pokemon.id}
+                  className="card p-2 mr-2"
+                  style={{ width: "130px", minWidth: "130px" }}
+                >
+                  <img src={pokemon.img} className="card-img-top" alt="..." />
+                  <div className="card-body m-0 p-0">
+                    <h6 className="card-title text-center">
+                      {`${pokemon.name} - ${getPokemontPoints(pokemon)}`}
+                    </h6>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col   d-flex justify-content-center">
+            <button
+              onClick={evaluatePokemons}
+              className="btn btn-primary w-50"
+              type="button"
+              disabled={chosenPokemon.length === 0}
+            >
               Avaliar possibilidade
             </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal-container"
+        style={{
+          width: openModal ? "100vw" : "0",
+          height: openModal ? "100vh" : "0",
+        }}
+      >
+        <div
+          className="custom-modal"
+          style={{
+            transform: openModal ? "translateY(0)" : "translateY(100rem)",
+          }}
+        >
+          <div className="row mb-4">
+            <div className="col">
+              <div className="pokemons-grid-modal">
+                {exchangedPokemons.map((pokemon) => (
+                  <div className="card p-2">
+                    <img src={pokemon.img} className="card-img-top" alt="..." />
+                    <div className="card-body m-0 p-0">
+                      <h6 className="card-title text-center">
+                        {`${pokemon.name} - ${getPokemontPoints(pokemon)}`}
+                      </h6>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              {exchangedPokemons.length === 0 && (
+                <h3 className="text-center">
+                  Não é possível fazer uma troca exata
+                </h3>
+              )}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col d-flex justify-content-center">
+              <button
+                className="btn btn-primary w-50"
+                type="button"
+                onClick={closeModal}
+              >
+                Fazer nova simulação
+              </button>
+            </div>
           </div>
         </div>
       </div>
